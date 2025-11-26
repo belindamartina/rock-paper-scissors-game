5f42dff175aff82db2815ce63802afb0160e2a12
Belinda Martina <159711237+belindamartina@users.noreply.github.com>
2025-11-27 02:38:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function getComp(){
+    //get comp choice
+    var random = Math.random();
+    if (random<0.33) return "rock";
+    if (random>=0.33 && random<=0.66) return "paper";
+    return "scissor";
+};
+
+function getResult(comp,player){
+    //get result
+    if (player==comp) return "TIE";
+    if (player=="rock") return (comp=="paper")?"YOU LOSE":"YOU WIN";
+    if (player=="paper") return (comp=="rock")?"YOU WIN":"YOU LOSE";
+    if (player=="scissor") return (comp=="rock")?"YOU LOSE":"YOU WIN";
+}
+
+//get button and picture element
+var rock = document.querySelector('.rock');
+var paper = document.querySelector('.paper');
+var scissor = document.querySelector('.scissor');
+var imgPlayer = document.querySelector('.q1')
+var imgComp = document.querySelector('.q2')
+var result = document.querySelector('.res')
+
+//add event on elements
+rock.addEventListener('click', function(){
+    const compOpt = getComp();
+    const playerOpt = rock.className;
+    const fin = getResult(compOpt, playerOpt);
+    imgPlayer.setAttribute("src", "rock.png");
+    imgComp.setAttribute("src", compOpt + ".png");
+    result.innerHTML = fin;
+});
+
+paper.addEventListener('click', function(){
+    const compOpt = getComp();
+    const playerOpt = paper.className;
+    const fin = getResult(compOpt, playerOpt);
+    imgPlayer.setAttribute("src", "paper.png");
+    imgComp.setAttribute("src", compOpt + ".png");
+    result.innerHTML = fin;
+
+});
+
+scissor.addEventListener('click', function(){
+    const compOpt = getComp();
+    const playerOpt = scissor.className;
+    const fin = getResult(compOpt, playerOpt);
+    imgPlayer.setAttribute("src", "scissor.png");
+    imgComp.setAttribute("src", compOpt + ".png");
+    result.innerHTML = fin;
+});
